4d7021d60d4764223b26b9feac12f91a416bf704
willegge <willeggedev@gmail.com>
2024-07-10 14:53:26 -0400

    chore: Add login schema for sign in form

---
+++ app/login/schema.ts
@@ -0,0 +1,10 @@
+// app/signin/schema.ts
+
+import { z } from "zod";
+
+export const signInSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+export type SignInFormData = z.infer<typeof signInSchema>;
