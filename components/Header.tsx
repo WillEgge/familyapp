85f85510cfbb56c60bd46cbd7239a158dec4b10b
willegge <willeggedev@gmail.com>
2024-07-10 14:29:39 -0400

    chore: Refactor Header component and add sign out functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,44 +1,50 @@
-import NextLogo from "./NextLogo";
-import SupabaseLogo from "./SupabaseLogo";
+import { signOut } from "@/app/login/actions";
+import { Button } from "@/components/ui/button";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
 
-export default function Header() {
+export default async function Header() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto px-4 py-6 flex items-center justify-between">
+        <div className="flex items-center">
+          <Link href="/">
+            <span className="text-3xl font-bold text-indigo-600">
+              FamilyHub
+            </span>
+          </Link>
+        </div>
+        <nav className="flex items-center space-x-8">
+          <Link href="/tasks">
+            <span className="text-gray-600 hover:text-indigo-600">Tasks</span>
+          </Link>
+          <Link href="/addchild">
+            <span className="text-gray-600 hover:text-indigo-600">
+              Add Child
+            </span>
+          </Link>
+          {user !== null ? (
+            <form className="flex flex-col items-center" action={signOut}>
+              <button className="ml-8 inline-block bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded">
+                {user.email}
+              </button>
+              <span className="text-center mt-1 text-xs text-gray-400">
+                Click to sign out
+              </span>
+            </form>
+          ) : (
+            <Link href="/login">
+              <Button>Sign In</Button>
+            </Link>
+          )}
+        </nav>
       </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
-      </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
-    </div>
+    </header>
   );
 }
