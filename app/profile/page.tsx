9300dedaa594da8c137e2b8de5be3decfc59d2f8
willegge <willeggedev@gmail.com>
2024-07-16 17:09:41 -0400

    refactor: Add ProfilePage component to Profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,11 @@
+import ProfilePage from "@/components/ProfilePage";
+
+export default function Profile() {
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="w-full">
+        <ProfilePage />
+      </div>
+    </div>
+  );
+}
