5bf824991d7099f8f2c5d96700419b1f86b856c9
willegge <willeggedev@gmail.com>
2024-07-25 16:01:31 -0400

    refactor: Update LayoutWrapper component to include SideNavToggle and adjust layout structure

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -6,12 +6,9 @@ import { SideNavToggle } from "@/components/SideNavToggle";
 export function LayoutWrapper({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
+      <SideNavToggle />
       <SideNav />
-      <div className="flex-1">
-        <header className="p-4 flex items-center justify-between">
-          <SideNavToggle />
-          {/* Add other header content here */}
-        </header>
+      <div className="flex-1 layout-content">
         <main className="p-4">{children}</main>
       </div>
     </div>
