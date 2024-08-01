2813f7639928f2672f59a15474916fd4f1a59bd1
willegge <willeggedev@gmail.com>
2024-08-01 17:04:06 -0400

    refactor: Update LayoutWrapper component to include sidebar functionality

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,13 +1,19 @@
 "use client";
 
+import { useState } from "react";
 import { SideNav } from "@/components/SideNav";
 import { SideNavToggle } from "@/components/SideNavToggle";
 
 export function LayoutWrapper({ children }: { children: React.ReactNode }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+  const closeSidebar = () => setIsSidebarOpen(false);
+
   return (
-    <div className="flex">
-      <SideNavToggle />
-      <SideNav />
+    <div className={`flex ${isSidebarOpen ? "sidebar-open" : ""}`}>
+      <SideNavToggle isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <SideNav isOpen={isSidebarOpen} closeSidebar={closeSidebar} />
       <div className="flex-1 layout-content">
         <main className="p-4">{children}</main>
       </div>
