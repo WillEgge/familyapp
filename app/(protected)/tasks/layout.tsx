7ad0f11b9cce01b0e3dae3b6324ac5808a14f835
willegge <willeggedev@gmail.com>
2024-08-08 16:11:03 -0400

    refactor: Update TasksLayout component to support touch devices for drag and drop functionality

--- app/(protected)/tasks/layout.tsx
+++ app/(protected)/tasks/layout.tsx
@@ -1,13 +1,24 @@
-// app/(protected)/tasks/layout.tsx
 "use client";
 
 import { DndProvider } from "react-dnd";
+import { TouchBackend } from "react-dnd-touch-backend";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { useEffect, useState } from "react";
 
 export default function TasksLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <DndProvider backend={HTML5Backend}>{children}</DndProvider>;
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
+
+  useEffect(() => {
+    setIsTouchDevice("ontouchstart" in window || navigator.maxTouchPoints > 0);
+  }, []);
+
+  return (
+    <DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend}>
+      {children}
+    </DndProvider>
+  );
 }
