efe00aa4928ca63d2fb372466f35eab6d17f5e78
willegge <willeggedev@gmail.com>
2024-08-27 10:58:24 -0400

    refactor: Add LayoutWrapper component to TasksLayout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -5,13 +5,14 @@ import { TouchBackend } from "react-dnd-touch-backend";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { useEffect, useState } from "react";
 import VersionInfo from "@/components/VersionInfo";
+import { LayoutWrapper } from "@/components/LayoutWrapper";
 
 const touchBackendOptions = {
   enableMouseEvents: true,
-  delayTouchStart: 100, // in milliseconds
+  delayTouchStart: 100,
 };
 
-export default function TasksLayout({
+export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
@@ -27,7 +28,7 @@ export default function TasksLayout({
       backend={isTouchDevice ? TouchBackend : HTML5Backend}
       options={isTouchDevice ? touchBackendOptions : undefined}
     >
-      {children}
+      <LayoutWrapper>{children}</LayoutWrapper>
       <VersionInfo />
     </DndProvider>
   );
