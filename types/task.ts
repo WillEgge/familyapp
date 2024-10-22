91e19e9f1310df1094a01b9d4352515adb3d678d
willegge <willeggedev@gmail.com>
2024-10-22 17:01:22 -0400

    new types test

--- types/task.ts
+++ types/task.ts
@@ -1,6 +1,8 @@
 export interface Task {
   task_id: string | number;
   title: string;
+ // position: number;
+  //column_id: string;
   description: string | null;
   due_date: string;
   priority: number;
@@ -9,3 +11,23 @@ export interface Task {
   order: number;
   recurrence: "none" | "daily" | "weekly" | "monthly" | "yearly" | null;
 }
+
+// export type ColumnType = {
+//   id: string;
+//   title: string;
+//   tasks: Task[];
+// };
+
+// export type Board = {
+//   title: string;
+//   columns: ColumnType[];
+// };
+
+// export type BoardContextType = {
+//   board: Board;
+//   moveTask: (
+//     id: number,
+//     targetColumnId: string,
+//     targetPosition: number
+//   ) => void;
+// };
